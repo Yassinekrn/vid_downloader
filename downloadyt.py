10583eb8618ea63175be48cfcfcadef28d0869ff
Yassinekrn <krichenyassine22@gmail.com>
2022-12-25 00:47:49 +0100

    added mp3 option

--- downloadyt.py
+++ downloadyt.py
@@ -17,33 +17,49 @@ def select_path():
 
 
 def download_file():
+    # type of download
+    format_value = format_var.get()
     # get user path
     get_link = link_field.get()
     # get selected path
     user_path = path_label.cget("text")  # gets path as txt
     screen.title("Downloading, Please Wait...")
     # Download video
-    mp4_video = YouTube(get_link).streams.get_highest_resolution().download()
-    vid_clip = VideoFileClip(mp4_video)
-    vid_clip.close()
-    # move to selected dir
-    shutil.move(mp4_video, user_path)
+    if format_value == 1:
+        audio_file = YouTube(get_link).streams.get_audio_only().download()
+        shutil.move(audio_file, user_path)
+
+    elif format_value == 2:
+        mp4_video = YouTube(
+            get_link).streams.get_highest_resolution().download()
+        vid_clip = VideoFileClip(mp4_video)
+        vid_clip.close()
+        # move to selected dir
+        shutil.move(mp4_video, user_path)
     screen.title("Download Complete! Try downloading another file.")
 
 
 def download_pl():
+    # type of download
+    format_value = format_var.get()
     # get user playlist link
     pl_link = pl_link_field.get()
     # get user path for downloads
     user_path = path_label.cget("text")  # gets path as txt
-
-    # loop to download
-    p = Playlist(pl_link)
-    for video in p.videos:
-        screen.title(f'Downloading: {video.title}')
-        st = video.streams.get_highest_resolution().download()
-        # move to selected dir
-        shutil.move(st, user_path)
+    if format_value == 2:
+        # loop to download
+        p = Playlist(pl_link)
+        for video in p.videos:
+            screen.title(f'Downloading: {video.title}')
+            st = video.streams.get_highest_resolution().download()
+            # move to selected dir
+            shutil.move(st, user_path)
+    elif format_value == 1:
+        p = Playlist(pl_link)
+        for video in p.videos:
+            screen.title(f'Downloading: {video.title}')
+            audio_file = video.streams.get_audio_only().download()
+            shutil.move(audio_file, user_path)
 
         screen.title("Download Complete! Try downloading another file.")
 
@@ -79,19 +95,26 @@ path_label = Label(screen, text="Select Path: ",
 
 select_btn = Button(screen, text="Select", command=select_path)
 # Add to window
-canvas.create_window(250, 300, window=path_label)
-canvas.create_window(250, 335, window=select_btn)
+canvas.create_window(250, 330, window=path_label)
+canvas.create_window(250, 366, window=select_btn)
+
+# Add checkbutton mp3 - mp4
+format_var = IntVar()
+mp3_radio = Radiobutton(text="MP3", variable=format_var, value=1)
+canvas.create_window(175, 290, window=mp3_radio)
+mp4_radio = Radiobutton(text="MP4", variable=format_var, value=2)
+canvas.create_window(325, 290, window=mp4_radio)
 
 
 # add widget to window
-canvas.create_window(250, 365, window=link_label)
-canvas.create_window(250, 400, window=link_field)
+canvas.create_window(250, 405, window=link_label)
+canvas.create_window(250, 435, window=link_field)
 
 # download btns
 download_btn = Button(screen, text="Download File", command=download_file)
 pl_download_btn = Button(screen, text="Download Playlist", command=download_pl)
 
 # add to canvas
-canvas.create_window(250, 440, window=download_btn)
+canvas.create_window(250, 465, window=download_btn)
 canvas.create_window(250, 565, window=pl_download_btn)
 screen.mainloop()
